73f56c08cc61dae247d306887d9c1ae8509c3e75
topstech <topstech@DESKTOP-BENL1HS>
2024-01-02 08:52:17 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBasics.Controllers
+{
+    //[RoutePrefix("yogesh")]
+    public class StudentController : Controller
+    {
+        [Route("200")]
+        public string MyName()
+        {
+            return "mv example";
+        }
+
+        // GET: Student
+        public ActionResult ViewStudents()
+        {
+            return View();
+        }
+
+        public ActionResult Test(string name)
+        {
+            object obj = name;
+            if (name != null)
+                return View("ViewStudents", obj);
+            else
+                return View();
+        }
+    }
+}
